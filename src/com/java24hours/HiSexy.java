469be7f03649888fdd3f6c9ad976ff0f071b8ed8
Robert McKeithen <rmckeithen41@gmail.com>
2018-10-31 00:22:43 -0400

    Message Project

---
+++ src/com/java24hours/HiSexy.java
@@ -0,0 +1,11 @@
+package com.java24hours;
+
+class HiSexy {
+    public static void main(String[] arguments) {
+        String greeting = "Hi Sexy!!! ";
+        System.out.println(greeting);
+        String compliment = "Your'e looking very pretty today ";
+        System.out.println
+        (compliment);       
+    }
+}
